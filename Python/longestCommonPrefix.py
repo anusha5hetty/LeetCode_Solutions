3f3414e428e166ea1357a3bee74441fecbd764ba
Anusha Shetty <41414770+ashetty-planview@users.noreply.github.com>
2024-10-10 20:49:03 +0530

    Solution for two-sum

---
+++ Python/longestCommonPrefix.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problemset/
+# https://leetcode.com/problems/two-sum/description/
+from typing import List
+
+
+
+
+
+class Solution:
+
+    def longestCommonPrefixMySolution(self, strs: List[str]) -> str:
+        output = ""
+
+        if len(strs) == 0 or len(strs[0]) == 0:
+            return output
+        
+        for idx in range(200):
+            try:
+                letter = strs[0][idx]
+
+                for string in strs:
+                    if string == "" or (len(string) <= idx) or (string[idx] != letter):
+                        return output
+
+                output += letter
+            except Exception:
+                return output
+
+    def longestCommonPrefixBestSolution(self, strs: List[str]) -> str:
+        strs = sorted(strs)
+        result = ""
+        first = strs[0]
+        last = strs[-1]
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:
+                return result
+            result += first[i]
+        return result
