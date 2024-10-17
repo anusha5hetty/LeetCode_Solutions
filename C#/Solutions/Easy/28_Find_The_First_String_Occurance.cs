2a98748dd91e686d4cc7b1607fb953811025544b
Anusha Shetty <shettya092@gmail.com>
2024-10-17 09:20:56 +0530

    Moved some files around

---
+++ C#/Solutions/Easy/28_Find_The_First_String_Occurance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.Easy
+{
+  internal class _28_Find_The_First_String_Occurance
+  {
+    public static int StrStr(string haystack, string needle)
+    {
+      for(int idx=0; idx < haystack.Length; idx++)
+      {
+        if (haystack[idx] == needle[0] && SubstringExists(haystack, idx, needle))
+        {
+          return idx+1;
+        }
+      }
+      return -1;
+    }
+
+    private static bool SubstringExists(string haystack, int haystackIdx, string needle)
+    {
+      for (int idx = 0; idx < needle.Length; idx++)
+      {
+        if(haystack[haystackIdx] != needle[idx])
+          return false;
+        haystackIdx++;
+      }
+      return true;
+    }
+  }
+}
---
+++ C#/Solutions/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Solutions.Easy;
+
+var haystack = "hello";
+var needle = "lo";
+Console.WriteLine(_28_Find_The_First_String_Occurance.StrStr(haystack, needle));
