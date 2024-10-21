2cba17d8234272f7d096b514a7b2f59144e14e69
Anusha Shetty <shettya092@gmail.com>
2024-10-21 22:12:45 +0530

    Update 66_Plus_One.py

--- Python/Easy/66_Plus_One.py
+++ Python/Easy/66_Plus_One.py
@@ -4,35 +4,31 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        str_digits = ''.join(map(str, digits))
+        int_digits = int(str_digits) + 1
+        return list(map(int, str(int_digits)))
+    
+    def plusOne2(self, digits: List[int]) -> List[int]:
         idx = len(digits)-1
+        remainder = 1
 
-        digits[idx] += 1
-        remainder = digits[idx] % 9
 
-        while remainder>0 and idx > 0:
-            idx -= 1
-            digits[idx] = digits[idx] + remainder
+        while remainder>0 and idx >= 0:
+            digits[idx] += remainder
+            remainder = digits[idx] % 9
 
             if digits[idx]>9:
                 digits[idx] = digits[idx] % 10
                 remainder = 1
             else:
                 remainder = 0
-            
+            idx -= 1
         
-        if remainder>0:
-            return [remainder].extend(digits)
-        else :
-            return digits
+        if remainder == 1:
+            digits.insert (0, remainder)
+        return digits
 
-            
-
-    
-    def plusOne2(self, digits: List[int]) -> List[int]:
-        str_digits = ''.join(map(str, digits))
-        int_digits = int(str_digits) + 1
-        return list(map(int, str(int_digits)))
-    
-print(Solution().plusOne([9, 9]))
+               
+print(Solution().plusOne2([1, 9, 9]))
 
 
