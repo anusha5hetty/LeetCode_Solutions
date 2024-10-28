7ed123d9c7c5a96554cddf8004e8787a7cfe634a
Anusha Shetty <shettya092@gmail.com>
2024-10-28 23:41:24 +0530

    Create 20_ ValidParentheses.cs

---
+++ C#/Solutions/Easy/20_ ValidParentheses.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Easy  
+{
+  internal class _20__ValidParentheses
+  {
+    private static Dictionary<string, string> mappings = new Dictionary<string, string>() { { "(",")" }, { "{","}" }, { "[", "]" } };
+    private IReadOnlyDictionary<string, string> readonlyDict = new ReadOnlyDictionary<string, string>(mappings);
+
+    public bool isValid(string s)
+    {
+      var stack = new Stack();
+      foreach(char x in s)
+      {
+        var strX = x.ToString();
+        if (readonlyDict.ContainsKey(strX))
+          stack.Push(strX);
+
+        else if (stack.Count == 0 || (!strX.Equals(readonlyDict[stack.Pop().ToString()])))
+            return false;
+      }
+
+      return stack.Count == 0;
+    }
+  }
+}
