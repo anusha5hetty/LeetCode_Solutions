fd6d0ab1dd6e4a6a3a933f9bca452f597d617adb
Anusha Shetty <shettya092@gmail.com>
2024-11-03 15:05:04 +0530

    Create 11_Container_With_Most_Water.cs

---
+++ C#/Solutions/Medium/11_Container_With_Most_Water.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.Medium
+{
+  internal class _11_Container_With_Most_Water
+  {
+    public static int maxArea(int[] height)
+    {
+      int mostWater=0, water=0, firstIdx = 0;
+      int lastIdx = height.Length - 1;
+      while (firstIdx < lastIdx)
+      {
+        int gap = lastIdx - firstIdx;
+        if (height[firstIdx] < height[lastIdx])
+        {
+          water = gap * height[firstIdx++]; // firstIdx is incremented after the current value is used with height. Equivalent to height[firstIdx]; firstIdx++;
+        }
+        else
+        {
+          water = gap * height[lastIdx--];
+        }
+
+        if(mostWater < water)
+        {
+          mostWater = water;
+        }
+
+      }
+      return mostWater;
+    }
+  }
+}
