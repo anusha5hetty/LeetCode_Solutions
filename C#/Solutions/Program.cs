9cb755e844b91f87ac165086a7c97b4107a74b69
Anusha Shetty <shettya092@gmail.com>
2024-11-14 22:16:56 +0530

    Update Program.cs

--- C#/Solutions/Program.cs
+++ C#/Solutions/Program.cs
@@ -17,4 +17,6 @@ using Solutions.Medium;
 
 //Console.WriteLine(_14_Longest_Common_Prefix.longestCommonPrefix(strs));
 
-Console.WriteLine(_11_Container_With_Most_Water.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+//Console.WriteLine(_11_Container_With_Most_Water.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+
+Console.WriteLine(_119_Pascals_Triangle_2.getRow(0));
