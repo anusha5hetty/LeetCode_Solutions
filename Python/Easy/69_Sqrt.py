d31d408d207ceadcc2e387df2ac73207e5b5475d
Anusha Shetty <shettya092@gmail.com>
2024-12-19 22:48:38 +0530

    Create 69_Sqrt.py

---
+++ Python/Easy/69_Sqrt.py
@@ -0,0 +1,23 @@
+import math
+
+class Solution:
+    def mySqrtSimple(self, x: int) -> int:
+        return math.floor(math.sqrt(x))
+    
+    def mySqrtActual(self, x: int) -> int:
+        if x == 0 or x == 1:
+            return x
+        start = 1
+        end = x//2
+        midIndex = end//2
+
+        # midIndex**2 calculates the power of 2
+        while start <= end and midIndex**2 != x:
+            if midIndex**2 > x:
+                end = midIndex - 1
+            else:
+                start = midIndex + 1
+            midIndex = (start + end)//2
+        return midIndex
+
+Solution().mySqrtActual(81)
