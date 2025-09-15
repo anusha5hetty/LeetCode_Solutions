0bd5e19b17d752ab8cd4737fc66bf45e1b91d65a
Anusha Shetty <shettya092@gmail.com>
2025-09-15 21:26:34 +0530

    Group Anagram

---
+++ C#/Solutions/Medium/27_Group_Anagrams.cs
@@ -0,0 +1,31 @@
+﻿namespace Solutions.Medium;
+
+public class _27_GroupAnagramsClass
+{
+  public static IList<IList<string>> GroupAnagrams(string[] strs)
+  {
+    var store = new Dictionary<string, IList<string>>();
+    foreach (var item in strs)
+    {
+      var uniqueKey = new int[26];
+      foreach(char ch in item)
+      {
+        uniqueKey[ch - 'a'] = 1;
+      }
+
+      var keyString = string.Join(",", uniqueKey);
+
+      if (store.TryGetValue(keyString, out var existingGroup))
+      {
+        existingGroup.Add(item);
+      }
+      else
+      {
+        store[keyString] = new List<string> { item };
+      }
+
+    }
+    return store.Values.ToList();
+
+  }
+}
